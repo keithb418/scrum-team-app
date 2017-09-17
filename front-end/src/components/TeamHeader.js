0e91fd487eab8d957b8ef176c2f81fdbee9fdec2
usman-tahir <tahir.usman.ali94@outlook.com>
2017-09-17 15:13:30 -0400

    feat: initial attempt at the Team Header component (WIP)

---
+++ front-end/src/components/TeamHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, Row, Col } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
+
+const TeamHeader = ({_id: id, teamName}) => {
+    return(
+        <Button className="team-header">
+            <Row>
+                <Col xs={2}>
+                    <FontAwesome name="user-add"/>
+                </Col>
+            </Row>
+        </Button>
+    )
+}
