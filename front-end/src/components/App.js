732ab0f402589c7897a9d09b1a89d6f19f177509
Ansoo <asc2683@gmail.com>
2017-09-20 19:51:02 -0400

    feature: header component (missing style)

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Header from './Header'
 
 const App = () => (
   <div>
+    <Header />
     <h1 className="title">Hello World!  I will be the Scrum Team app!</h1>
   </div>
 );
---
+++ front-end/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Scrum Team</h1>
+    </header>
+  )
+}
+
+export default Header
