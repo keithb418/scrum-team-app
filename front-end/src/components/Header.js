941bfbf5eed0f7471f7b660fb0d602bbf2c70dcf
Ansoo <asc2683@gmail.com>
2017-09-20 20:43:02 -0400

    feat: header component (basic minimal styling)

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <header>
-      <h1>Scrum Team</h1>
+      <Row>
+        <Col xs={12}>
+          <div className="page-header">
+            <h1>Scrum Team</h1>
+          </div>
+        </Col>
+      </Row>
     </header>
   )
 }
