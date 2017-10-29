365ad578122fd48cbc6d444b0c3ef17f55df6d76
ray.waheed <ray.waheed@raywaheeds-MacBook-Pro.local>
2017-10-29 12:15:50 -0400

    adding route reducer

---
+++ front-end/src/reducers/routeReducer.js
@@ -0,0 +1,10 @@
+const route = (state = "", action) => {
+    switch (action.type) {
+      case "CHANGE_ROUTE":
+      	return action.route;
+      default:
+        return state;
+    }
+  };
+  
+  export default route;
