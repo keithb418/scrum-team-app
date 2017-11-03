451a4bc31f865d77d487d4800a21186ce6515d9c
Ansoo <asc2683@gmail.com>
2017-11-02 22:45:44 -0400

    test: header compnent

---
+++ __tests__/components/Header.test.js
@@ -0,0 +1,17 @@
+import Header from "../../front-end/src/components/Header";
+
+import React from "react";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-15";
+import { shallow } from "enzyme";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("Header component", () => {
+  it("should have h1 tag with Scrum Team", () => {
+    const wrapper = shallow(
+      <Header />
+    );
+    expect(wrapper.contains(<h1>Scrum Team</h1>)).toBe(true);
+  });
+});
