ce75c1e6d41b533a3be1ec9fb01e276a4ce0df2a
Crucnhwrap78 <Hayesapples78@gmail.com>
2018-04-05 19:05:10 -0400

    cleanup

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,97 +3,94 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
-const CompressionPlugin = require("compression-webpack-plugin");
-const BrotliPlugin = require("brotli-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
 
-const VENDORS = [
-  'react', 'react-dom', 'redux', 'redux-thunk', 'lodash', 'uuid',
-  'react-redux', 'react-bootstrap', 'react-fontawesome', 'axios'
-];
 module.exports = {
-  entry: {
-    main: [
-      'babel-polyfill', 
-      path.join(__dirname, './front-end/src/index.js')
-    ],
-  },
-  optimization: {
+	entry: {
+		main: ['babel-polyfill', path.join(__dirname, './front-end/src/index.js')],
+	},
+	optimization: {
 		splitChunks: {
 			cacheGroups: {
 				vendor: {
 					test: /node_modules/,
-					chunks: "initial",
-					name: "vendor",
+					chunks: 'initial',
+					name: 'vendor',
 					priority: 10,
-					enforce: true
-				}
-			} 
-		}
+					enforce: true,
+				},
+			},
+		},
 	},
-  mode: "production",
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name]-bundle.js',
-    chunkFilename: '[name].js', 
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      { 
-        test: /\.svg$/, 
-        loader: 'svg-loader' 
-      },
-      {
-        test: /\.(jpg|png|ico|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: './img/[name].[ext]',
-        },
-      },
-      {
-        test: /\.json?$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader!sass-loader",
-          publicPath: "/"
-        })
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'front-end/index.tpl.html',
-      inject: 'body',
-      filename: 'index.html'
-    }),
-    new ExtractTextPlugin('[name].css'),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.css$/g,
-      cssProcessor: require("cssnano"),
-      cssProcessorOptions: { discardComments: { removeAll: true } },
-      canPrint: true
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new UglifyJSPlugin(),
-    new CompressionPlugin({
-      algorithm: "gzip"
-    }),
-    new BrotliPlugin()
-  ],
+	mode: 'production',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: '[name]-bundle.js',
+		chunkFilename: '[name].js',
+		publicPath: '/',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.svg$/,
+				loader: 'svg-loader',
+			},
+			{
+				test: /\.(jpg|png|ico|svg)$/,
+				loader: 'file-loader',
+				options: {
+					name: './img/[name].[ext]',
+				},
+			},
+			{
+				test: /\.json?$/,
+				loader: 'json-loader',
+			},
+			{
+				test: /\.scss$/,
+				loader: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader!sass-loader',
+					publicPath: '/',
+				}),
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'front-end/index.tpl.html',
+			inject: 'body',
+			filename: 'index.html',
+		}),
+		new ExtractTextPlugin('[name].css'),
+		new OptimizeCssAssetsPlugin({
+			assetNameRegExp: /\.css$/g,
+			cssProcessor: require('cssnano'),
+			cssProcessorOptions: {
+				discardComments: {
+					removeAll: true,
+				},
+			},
+			canPrint: true,
+		}),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+		}),
+		new UglifyJSPlugin(),
+		new CompressionPlugin({
+			algorithm: 'gzip',
+		}),
+		new BrotliPlugin(),
+	],
 };
